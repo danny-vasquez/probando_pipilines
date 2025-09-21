104b35d6c434ceb6e9ff2d81213e7a907262020d
Danny <dj.vasquez@uniandes.edu.co>
2025-09-20 23:05:57 -0500

    cambio random

--- src/mi_proyecto/persona.py
+++ src/mi_proyecto/persona.py
@@ -17,7 +17,7 @@ class Persona:
        return(self.__edad)
 
    def dar_nombre(self):
-       return(self.__nombre)
+       return(self.__nombre) 
 
    def calcular_anio_nacimiento(self, ya_cumplio_anios):
        anio_actual = datetime.datetime.now().year
